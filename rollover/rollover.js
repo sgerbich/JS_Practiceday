0e70b260e10c73e861054e86603b6ebdf45c6b35
unknown <sdgerbich@gmail.com>
2020-04-09 14:04:18 -0400

    working rollover

--- rollover/rollover.js
+++ rollover/rollover.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){ 
 
-$("div").hover(function(){
-    $("p").remove();
-    $(this).append( $("<p>over</p>"));
-}, function(){
+$("div").mouseover(function(){
+    // $("p").remove();
+    $(this).find(".words").text("over");
+}) .mouseout(function(){
     
     // $("p").remove();
-    $(this).append( $("<p>out</p>"));
+    $(this).find(".words").text("out");
 });
 });
